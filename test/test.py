48a449f8e9dbac5b24dbceec28ce05077ab2689e
magnetofield <34056090+magnetoField@users.noreply.github.com>
2026-05-15 18:09:44 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -53,12 +53,14 @@ async def test_project(dut):
 
     # Reset the design
     dut.ena.value = 1
-    dut.ui_in.value = 255
+    dut.ui_in.value = 20
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.ui_in.value=0
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 20)
+    dut.ui_in.value = 20
     await ClockCycles(dut.clk, 200)
 
     # Define some functions for capturing lines & frames
